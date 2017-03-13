e8ce6ef4e43da8e084a973b079027355f8f132e4
Jeff Hawkins <hawkins.jeffm@gmail.com>
2017-03-12 17:58:51 -0700

    cleaned up files

--- positive_tweet.py
+++ positive_tweet.py
@@ -1,92 +1,99 @@
-from collections import defaultdict
-import re
-import en
-from random import randint
-from nltk import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import wordnet
-from nltk.tokenize.moses import MosesDetokenizer
 import HTMLParser
 
+import re
 
-#This function takes a tweet and returns text of the same tweet, but with all the negative sentiment words replaced with positive sentiment ones
+from collections import defaultdict
+
+from random import randint
+
+import en
+
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize.moses import MosesDetokenizer
+
+# This function takes a tweet and returns text of the same tweet, but
+# with all the negative sentiment words replaced with a random positive
+# sentiment word matching it's part of speech(POS).
+
+
 def positive_tweet(tweet):
-    #flag to note if any changes were made
+    # Flag to note if any changes were made
     change_made = False
-    text = word_tokenize(tweet.text)
-    #tags words by pos - (word, pos)
+    # Tags words by pos - (word, pos)
     tagged = en.sentence.tag(tweet.text)
-    lemmatizer = WordNetLemmatizer()
-    
     mod_tweet = []
+
     for word in tagged:
         mod_word = check_replace(word[0], word[1], neg_words)
         mod_tweet.append(mod_word[0])
-        if mod_word[1] == True:
+        if mod_word[1] is True:
             change_made = True
 
     detokenizer = MosesDetokenizer()
     final = detokenizer.detokenize(mod_tweet, return_str=True)
     final = HTMLParser.HTMLParser().unescape(final)
-    
+
     return [final, change_made]
 
-#All other functions act in support of positive_tweet
+# All other functions act in support of positive_tweet.
 
-#import sentiment word list as list of dicts
-#From http://mpqa.cs.pitt.edu/lexicons/subj_lexicon/
-#see their license
+# Imports sentiment word list as list of dicts.
+# Word list comes from http://mpqa.cs.pitt.edu/lexicons/subj_lexicon/
+# See their license for use.
 list_tff = 'subjectivity_clues_hltemnlp05/subjclueslen1-HLTEMNLP05.txt'
 tags = ['type', 'pos1', 'stemmed1', 'priorpolarity']
 
-#parses the lines in the tff file containing words and sentiments
-#This does not work for capturing hyphenated words
+# Parses the lines in the tff file containing words and sentiments.
+# This does not work for capturing hyphenated words.
+
+
 def parse_tff(line, tags):
     features = {}
     for i in tags:
-        features[i] = re.findall(r"{}=(\w+)".format(i),line)[0]
+        features[i] = re.findall(r"{}=(\w+)".format(i), line)[0]
     return features
 
 
-#creat dict of features for negative words
+# Creates dict of features for negative words
 neg_dict = defaultdict(list)
 
-file_fft = open(list_tff) 
+file_fft = open(list_tff)
 for line in file_fft:
-    word = re.findall(r"{}=(.*) pos1".format('word1'),line)[0]
+    word = re.findall(r"{}=(.*) pos1".format('word1'), line)[0]
     features = parse_tff(line, tags)
     if features["priorpolarity"] == 'negative':
         neg_dict[word].append(features)
-        
-#create a list of negative words
+
+# Creates a list of negative words
 neg_words = neg_dict.keys()
-        
-#creat a dictionary of of positive words grouped by pos:
+
+# Creates a dictionary of of positive words grouped by POS.
 pos_dict = defaultdict(list)
 file_fft = open(list_tff)
 for line in file_fft:
-    pol = re.findall(r"{}=(\w+)".format('priorpolarity'),line)[0]
-    word = re.findall(r"{}=(.*) pos1".format('word1'),line)[0]
+    pol = re.findall(r"{}=(\w+)".format('priorpolarity'), line)[0]
+    word = re.findall(r"{}=(.*) pos1".format('word1'), line)[0]
     pos = en.sentence.tag(word)
     pos = pos[0][1]
     if pol == 'positive':
         pos_dict[pos].append(word)
 
 
-#translation dict
-#converts between varios part of speach (pos) formats
-tld = {'JJ':'adj',
-       'JJR':'adj',
-       'JJS':'adj',
-       'NN' : 'noun',
-       'NNP' : 'noun',
-       'NNPS' : 'noun',
-       'NNS' : 'noun',
-       'NNP' : 'noun',
-        'RB' : 'adverb',
-       'RBR' : 'adverb',
-       'RBS' : 'adverb',
-       'RP' : 'adverb',
+# Translation dict
+# Converts between various POS formats
+tld = {'JJ': 'adj',
+       'JJR': 'adj',
+       'JJS': 'adj',
+       'NN': 'noun',
+       'NNP': 'noun',
+       'NNPS': 'noun',
+       'NNS': 'noun',
+       'NNP': 'noun',
+       'RB': 'adverb',
+       'RBR': 'adverb',
+       'RBS': 'adverb',
+       'RP': 'adverb',
        'VA': 'verb',
        'VB': 'verb',
        'VBD': 'verb',
@@ -95,24 +102,25 @@ tld = {'JJ':'adj',
        'VBP': 'verb',
        'VBZ': 'verb',
        'VP': 'verb'
-      }
+       }
 
 
-#functions to determine if a word is negative and find a suitable replacement if it is
+# Functions to determine if a word is negative and find
+# a suitable replacement if it is.
 def check_replace(word, pos, neg_words):
-    #is it in the neg dict list?
+    # Is it in the neg dict list?
     mod_word = check_neg_pos_match(word, pos, neg_words, word)
-    #if the word doesn't match, try its lem.
-    if mod_word[0] == True:
+    # If the word doesn't match, try its lem.
+    if mod_word[0] is True:
         return [mod_word[1], True]
     else:
-        try: 
+        try:
             pos_lem = tld[pos]
             if pos_lem == 'verb':
                 lemmatizer = WordNetLemmatizer()
                 lem = lemmatizer.lemmatize(word, wordnet.VERB)
                 lem_match = check_neg_pos_match(lem, pos, neg_words, word)
-                if lem_match[0] == True:
+                if lem_match[0] is True:
                     return [lem_match[1], True]
                 else:
                     return [mod_word[1], False]
@@ -120,28 +128,26 @@ def check_replace(word, pos, neg_words):
                 return [mod_word[1], False]
         except:
             return [mod_word[1], False]
-    
-    
+
+
 def check_neg_pos_match(word_lem, pos_o, neg_words, original_word):
-    #flag to see if new word was chosen
-    nwc =None
-    word_l=word_lem.lower()
-    #is the word in the negative words list?
+    # Flag to see if new word was chosen.
+    nwc = None
+    word_l = word_lem.lower()
+    # Is the word in the negative words list?
     if word_l in neg_words:
-        #if so, do any entries for that word have the same pos?
+        # If so, do any entries for that word have the same pos?
         for features in neg_dict[word_l]:
             pos = features['pos1']
-            #print word_l, pos_o, pos
-            #for a matching pos, then return the word and matching pos
+            # For a matching pos, then return the word and matching pos
             if (pos == "anypos") or tld[pos_o] == pos:
-                #print word_l, pos_o, tld[pos_o], pol, pos
                 new_word = random_positive_word(pos_o, pos_dict)
-                #if it's a verb make sure the tense matches
+                # If it's a verb make sure the tense matches
                 if pos == 'verb':
-                    new_word = match_tense(new_word,original_word)
-                #make sure the capitalization matches
+                    new_word = match_tense(new_word, original_word)
+                # Make sure the capitalization matches
                 nwc = match_cap(new_word, original_word)
-        if nwc != None:
+        if nwc is not None:
             return [True, nwc]
         else:
             return [False, original_word]
@@ -149,44 +155,45 @@ def check_neg_pos_match(word_lem, pos_o, neg_words, original_word):
         return [False, original_word]
 
 
-
-#find similar positive word
-def  random_positive_word(pos, pos_dict):
-    #change mis-identified words to adjectives
+# Find a similar positive word.
+def random_positive_word(pos, pos_dict):
+    # Change mis-identified words to adjectives
     if pos == "NNP":
         pos = "JJ"
-    i = randint(0,len(pos_dict[pos])-1)
-    return pos_dict[pos][i]    
+    i = randint(0, len(pos_dict[pos]) - 1)
+    return pos_dict[pos][i]
 
-#function to correct the tense of any verbs being replaced
+
+# Function to correct the tense of any verbs being replaced.
 def match_tense(word, word_o):
-    
+
     tense = en.verb.tense(word_o)
 
-    test = {'past':en.verb.past(word),
-            '3rd singular present':en.verb.present(word, person='3'),
-            '3rd singular past':en.verb.past(word, person='3'),
-            'past participle':en.verb.past_participle(word),
-            'infinitive':en.verb.infinitive(word),
-            'present participle':en.verb.present_participle(word),
-            '1st singular present':en.verb.present(word, person='1'),
-            '1st singular past':en.verb.past(word, person='1'),
-            'past plural':en.verb.past(word, person='plural'),
-            '2nd singular present':en.verb.present(word, person='2'),
-            '2nd singular past':en.verb.past(word, person='2'),
-            'present plural':en.verb.present(word, person='plural')
-           }
-
+    test = {'past': en.verb.past(word),
+            '3rd singular present': en.verb.present(word, person='3'),
+            '3rd singular past': en.verb.past(word, person='3'),
+            'past participle': en.verb.past_participle(word),
+            'infinitive': en.verb.infinitive(word),
+            'present participle': en.verb.present_participle(word),
+            '1st singular present': en.verb.present(word, person='1'),
+            '1st singular past': en.verb.past(word, person='1'),
+            'past plural': en.verb.past(word, person='plural'),
+            '2nd singular present': en.verb.present(word, person='2'),
+            '2nd singular past': en.verb.past(word, person='2'),
+            'present plural': en.verb.present(word, person='plural')
+            }
     return test[tense]
 
-#function to match the capitialization used in the original tweet, either first letter capitalized or whole word
+
+# Function to match the capitialization used in the original tweet,
+# either first letter capitalized or whole word.
 def match_cap(out, test):
-    if test[0].isupper() == True:
-        if test[1].isupper() == True:
+    if test[0].isupper() is True:
+        if test[1].isupper() is True:
             caps = []
             for i in out:
                 caps.append(i.upper())
-            outcaps =  "".join(caps)
+            outcaps = "".join(caps)
             return outcaps
         else:
             return out[:1].upper() + out[1:]
--- realDonaldTrump.py
+++ realDonaldTrump.py
@@ -1,13 +1,14 @@
-import tweepy
-from positive_tweet import positive_tweet
-import HTMLParser
 import json
 
-# Import API Keys
-with open('positive_trump_creds.json') as creds:    
+from positive_tweet import positive_tweet
+
+import tweepy
+
+# Import API Keys.
+with open('positive_trump_creds.json') as creds:
     twitter_creds = json.load(creds)
-    
-#Access the twitter API
+
+# Access the twitter API.
 consumer_key = twitter_creds['consumer_key']
 consumer_secret = twitter_creds['consumer_secret']
 access_token = twitter_creds['access_token']
@@ -18,8 +19,7 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-#Listen to test account
-#username = 'realDonaldTrump'
+# Listen to test account.
 username = 'jhtestacc'
 user = api.get_user(username)
 
@@ -27,59 +27,52 @@ api_acc = api.me()
 print "Now listening to account: " + username
 print "Posting on account: " + api_acc.name
 
-#post positive version of tweet
-def post_tweet(original_tweet):    
-    n=0
+
+# Post positive version of tweet.
+def post_tweet(original_tweet):
+    n = 0
     ptweet = positive_tweet(original_tweet)
     change_made = ptweet[1]
     posttweet = ptweet[0]
-    if change_made == True:
-        #loop through and see if it comes up with a shorter tweet if over 140 characters
-        while n <100 and len(posttweet)>140:
-            n=n+1
+    if change_made is True:
+        # Loop through and see if it comes up with a shorter tweet
+        # if over 140 characters.
+        while n < 100 and len(posttweet) > 140:
+            n = n + 1
             ptweet = positive_tweet(original_tweet)
             change_made = ptweet[1]
             posttweet = ptweet[0]
     too_long = False
-    if len(posttweet)>140:
+    if len(posttweet) > 140:
         too_long = True
 
-#    print "orgingal tweet user id"
-#    print original_tweet.user.name
-#    print original_tweet.user.id
-#    print type(original_tweet.user.id)
-#    print"user id"
-#    print user.name
-#    print user.id
-#    print type(user.id)
-#    print"me id"
-#    print api_acc.name
-#    print api_acc.id
-#    print type(api_acc.id)
-
- #If the original tweet is the same as the modified one (no ngative words found), retweet the original.  Otherwise, post the modified tweet.
-    if change_made == False:
+    # If the original tweet is the same as the modified one
+    # (no ngative words found), retweet the original.
+    # Otherwise, post the modified tweet.
+    if change_made is False:
         print "Original Tweet was positive, so re-tweeted it."
         print "Original tweet text: " + original_tweet.text
         api.retweet(original_tweet.id)
-    elif too_long == False and change_made == True:
+    elif too_long is False and change_made is True:
         try:
             api.update_status(posttweet)
             print "New tweet posted"
             print "Original tweet text: " + original_tweet.text
             print "Positive tweet text: " + posttweet
         except:
-            print "All modified tweets were too long, no revised tweet posted."   
+            print "All modified tweets were too long, no revised tweet posted."
     else:
         print "No modified tweets that were short enough were found."
-        
-#Stream posts from twitter and post positive tweets
+
+
+# Stream posts from twitter and post positive tweets.
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        #when an event is detected print the text of the event and then post the positive tweet
+        # When an event is detected print the text of the event
+        # and then post the positive tweet.
         print("New tweet detected")
-    #detect retweets so we can skip them        
+        # Detect retweets so we can skip them
         retweet = False
         try:
             status.retweeted_status
@@ -87,22 +80,23 @@ class MyStreamListener(tweepy.StreamListener):
             print "Detected a RT, no further action"
         except AttributeError:
             pass
-#detect mentions so we can skip them 
+        # Detect mentions so we can skip them
         mention = False
         try:
             repl_id = status.in_reply_to_user_id_str
             if str(repl_id) == str(api_acc.id):
                 mention = True
-                print "Just a mention not a post by followed user. No further action"
+                print "Just a mention. No further action taken."
         except AttributeError:
             pass
-        
-        if retweet == False and mention == False and status.user.id != api_acc.id:
+
+        if (retweet is False and
+                mention is False and
+                status.user.id is not api_acc.id):
             post_tweet(status)
-        elif retweet == False and mention == False:
+        elif retweet is False and mention is False:
             print "Post was by this account. It will be ignored"
 
 myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
-#myStream.filter(follow=[str(user.id)])
+myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
 myStream.userstream("with=following")
